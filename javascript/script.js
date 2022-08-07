ce27851b6abfaebde7127f9a90e9754ffa76d8bc
Jordan Wright <jaws610@gmail.com>
2022-08-06 23:07:27 -0400

    Adding simple animation
    Added a simple animation for moving between sections of the portfolio.
    Trying to think of this as a SPA and am manipulating the site by hiding
    and showing different sections.

--- javascript/script.js
+++ javascript/script.js
@@ -1,2 +1,57 @@
-document.querySelector('.testing').addEventListener('click', () => console.log('hello world'));
+// document.getElementById('aboutMeNav').addEventListener('click', () => 
+// {   
+//     document.getElementById('aboutMe').classList.remove('show')
+//     document.getElementById('aboutMe').classList.add('hide')
+// });
 
+// document.getElementById('projectsNav').addEventListener('click', () =>{
+//     document.getElementById('projects').classList.add('show');
+// });
+
+let aboutMe = document.getElementById('aboutMe');
+let projects = document.getElementById('projects');
+
+sections = [aboutMe, projects]
+
+
+document.getElementById('aboutMeNav').addEventListener('click', showSection);
+document.getElementById('projectsNav').addEventListener('click', showSection);
+
+
+function showSection(e){
+    console.log(e.target);
+    console.log(e.target.innerHTML);
+    let sectionId;
+    let splitName = e.target.innerHTML.split(' ');
+
+    if (splitName[1]){
+        
+        splitName[0] = splitName[0].toLowerCase();
+        sectionId = splitName.join('');
+    } else {
+        sectionId = splitName[0].toLowerCase();
+    }
+
+    let something = document.getElementById(sectionId);
+    
+    if (something.classList.contains('show')){
+
+    } else {
+       
+        let stuff = document.getElementsByClassName('show')
+        let otherContainer = stuff[0];        
+        stuff[0].classList.replace('show', 'hide');
+
+        setTimeout(() => {
+            something.classList.replace('hide', 'show');
+            something.style.order = 0;
+            otherContainer.style.order=1;
+        }, 500);
+        
+        
+    
+
+        // something.classList.replace('hide', 'show');
+        
+    }
+}
