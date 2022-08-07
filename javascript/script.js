4fdadb1c1f41923dd4b90a5a6b575147fa31e919
Jordan Wright <jaws610@gmail.com>
2022-08-07 17:16:29 -0400

    Form Functionality
    Created a form that links with https://www.actionforms.io so that
    users can submit their contact info / messages to me via the contact me
    page. Also included an animation for the response message to show
    a positive or negative result.

--- javascript/script.js
+++ javascript/script.js
@@ -55,3 +55,82 @@ function showSection(e){
         
     }
 }
+
+let requestOptions = {
+  method: 'POST',
+  body: JSON.stringify({
+    title: 'Goodbye',
+    body: 'world',
+  }),
+  headers: {
+    'Content-Type': 'application/json'
+    // 'Content-Type': 'application/x-www-form-urlencoded',
+  },
+};
+
+// document.getElementById('formSubmit').addEventListener('click', (e) => {
+//     e.preventDefault()
+//     console.log('hello');
+//     fetch("https://www.actionforms.io/e/r/portfolio-forms", requestOptions)
+//     .then(response => response.text())
+//     .then(result => console.log(result))
+//     .catch(error => console.log('error', error));
+//   })
+
+document.getElementById('formSubmit').addEventListener('click', (e) => {
+    e.preventDefault()
+    console.log('hello');
+    let userName = document.getElementById('nameField').value;
+    let emailAddress = document.getElementById('emailField').value;
+    let message = document.getElementById('messageField').value;
+    console.log(userName, emailAddress, message);
+
+    let contactData = {
+        name: userName,
+        email: emailAddress,
+        message: message,
+    };
+
+    let requestPayload = {
+        method: 'POST',
+        body: JSON.stringify(contactData),
+        headers: {
+          'Content-Type': 'application/json'
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+    }
+
+    console.log('attempting post');
+    fetch("https://www.actionforms.io/e/r/portfolio-forms", requestPayload)
+        .then(response => {
+            if (response.status === 200) {
+                console.log('Yep!');               
+            }
+
+            showMessage(response.status)
+            return response.text()
+        })
+        .then(text => console.log(text))
+        .catch(error => console.log('error', error));
+
+    })
+
+    function showMessage(responseCode){
+        let messageContainer = document.getElementById('responseMessage')
+        if (responseCode === 200) {        
+            messageContainer.innerHTML = 'Thank You! Your Response Was Submitted'
+            messageContainer.style.color = '#CAD2C5'
+            messageContainer.style.display = 'inline'
+            messageContainer.classList.add('showMessage')
+
+            setTimeout( () => messageContainer.classList.replace('showMessage', 'hideMessage'), 2000)
+            
+        } else {
+            messageContainer.innerHTML = 'There was an error while submitting your form, please try again.'
+            messageContainer.style.color = 'red'
+            messageContainer.style.display = 'inline'
+        }
+    }
+
+    let messageContainer = document.getElementById('responseMessage')
+    // messageContainer.style.display = 'inline'
