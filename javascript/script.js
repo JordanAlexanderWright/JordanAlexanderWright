4860625c6539bfd477a8e2551ddf1346f4243728
Jordan Wright <jaws610@gmail.com>
2022-08-12 20:44:11 -0400

    Contact form responses causes issues, fixing container

--- javascript/script.js
+++ javascript/script.js
@@ -69,73 +69,72 @@ function showSection(e){
     }
 }
 
-let requestOptions = {
-  method: 'POST',
-  body: JSON.stringify({
-    title: 'Goodbye',
-    body: 'world',
-  }),
-  headers: {
-    'Content-Type': 'application/json'
-    // 'Content-Type': 'application/x-www-form-urlencoded',
-  },
-};
+// Handles the form submission
 
 document.getElementById('submitButton').addEventListener('click', (e) => {
     e.preventDefault()
-    console.log('hello');
+
     let userName = document.getElementById('nameField').value;
     let emailAddress = document.getElementById('emailField').value;
     let message = document.getElementById('messageField').value;
-    console.log(userName, emailAddress, message);
 
-    let contactData = {
-        name: userName,
-        email: emailAddress,
-        message: message,
-    };
+    if (!userName || !emailAddress || !message){
+        showMessage('Please fill out all fields');
+    } else {
 
-    let requestPayload = {
-        method: 'POST',
-        body: JSON.stringify(contactData),
-        headers: {
-          'Content-Type': 'application/json'
-          // 'Content-Type': 'application/x-www-form-urlencoded',
-        },
-    }
+        console.log(userName, emailAddress, message);
 
-    console.log('attempting post');
-    fetch("https://www.actionforms.io/e/r/portfolio-forms", requestPayload)
-        .then(response => {
-            if (response.status === 200) {
-                console.log('Yep!');               
-            }
+        let contactData = {
+            name: userName,
+            email: emailAddress,
+            message: message,
+        };
 
-            showMessage(response.status)
-            return response.text()
-        })
-        .then(text => console.log(text))
-        .catch(error => console.log('error', error));
+        let requestPayload = {
+            method: 'POST',
+            body: JSON.stringify(contactData),
+            headers: {
+            'Content-Type': 'application/json'
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+        }
 
-    })
+        console.log('attempting post');
+        fetch("https://www.actionforms.io/e/r/portfolio-forms", requestPayload)
+            .then(response => {
+                if (response.status === 200) {
+                    console.log('Yep!');               
+                }
+
+                showMessage(response.status)
+                return response.text()
+            })
+            .then(text => console.log(text))
+            .catch(error => console.log('error', error));
+
+    }    
+})
 
     function showMessage(responseCode){
         let messageContainer = document.getElementById('responseMessage')
         if (responseCode === 200) {        
-            messageContainer.innerHTML = 'Thank You! Your Response Was Submitted'
-            messageContainer.style.color = '#CAD2C5'
-            messageContainer.style.display = 'inline'
-            messageContainer.classList.add('showMessage')
+            messageContainer.innerHTML = 'Thank You! Your Response Was Submitted';
+            messageContainer.style.color = '#CAD2C5';
+            messageContainer.style.display = 'inline';
+            messageContainer.classList.add('showMessage');
 
-            setTimeout( () => messageContainer.classList.replace('showMessage', 'hideMessage'), 2000)
+            setTimeout( () => messageContainer.classList.replace('showMessage', 'hideMessage'), 2000);
             
         } else {
-            messageContainer.innerHTML = 'There was an error while submitting your form, please try again.'
-            messageContainer.style.color = 'red'
-            messageContainer.style.display = 'inline'
-            messageContainer.classList.add('showMessage')
+            messageContainer.innerHTML = 'An Error occured, please try again.';
+            messageContainer.style.color = 'red';
+            messageContainer.style.display = 'inline';
+            messageContainer.classList.add('showMessage');
 
-            setTimeout( () => messageContainer.classList.replace('showMessage', 'hideMessage'), 2000)
+            setTimeout( () => {
+                messageContainer.classList.replace('showMessage', 'hideMessage');
+                messageContainer.style.display = null;
+            }, 2000)
         }
     }
 
